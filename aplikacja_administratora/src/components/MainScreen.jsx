32ba895ffaf8b3dba17caa0f861f73ca81cc8135
BartlomiejJaruga <245827@edu.p.lodz.pl>
2024-05-05 16:34:20 +0200

    Upload wszystkich plików do stworzonej struktury

---
+++ aplikacja_administratora/src/components/MainScreen.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function MainScreen() {
+    return (
+        <>
+            <h1>Hello world!</h1>
+            <h2>First prototype of admin panel.</h2>
+        </>
+    )
+}
+
+export default MainScreen;
