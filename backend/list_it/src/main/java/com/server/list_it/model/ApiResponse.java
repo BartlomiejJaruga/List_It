5639852c30cc69d462587877019021157c04c07b
dziwicz123 <michal.dziwisz@poczta.onet.pl>
2024-05-18 15:26:01 +0200

    Back-end registration

---
+++ backend/list_it/src/main/java/com/server/list_it/model/ApiResponse.java
@@ -0,0 +1,13 @@
+package com.server.list_it.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse {
+    private String message;
+    private boolean status;
+}
---
+++ backend/list_it/src/main/java/com/server/list_it/repo/UserRepository.java
@@ -0,0 +1,8 @@
+package com.server.list_it.repo;
+
+import com.server.list_it.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    public User findByEmailAndPassword(String email, String password);
+}
