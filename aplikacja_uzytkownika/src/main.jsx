a8cdb846be99779d567b04ae0d6f436ff0e03aec
dziwicz123 <michal.dziwisz@poczta.onet.pl>
2024-05-18 21:22:02 +0200

    login front-end initialization

--- aplikacja_uzytkownika/src/main.jsx
+++ aplikacja_uzytkownika/src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './styles/index.css';
+import './styles/Login.css';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css'; // Import MDBReact CSS
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+);
