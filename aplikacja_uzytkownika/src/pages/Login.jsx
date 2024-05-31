4634ca8f9fc4df8b1d4717fc29c043dc1e63aa4c
dziwicz123 <michal.dziwisz@poczta.onet.pl>
2024-05-31 14:49:14 +0200

    correction of spelling

--- aplikacja_uzytkownika/src/pages/Login.jsx
+++ aplikacja_uzytkownika/src/pages/Login.jsx
@@ -60,7 +60,7 @@ function Login({ onLogin, onToggleRegister }) {
                         style={{ borderRadius: "1rem", maxWidth: "900px" }}
                     >
                         <Card.Body className="p-5 d-flex flex-column align-items-center mx-auto w-100">
-                            <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
+                            <h2 className="fw-bold mb-2 text-uppercase">Log in</h2>
                             <p className="text-white-50 mb-3">
                                 Please enter your email and password to login.
                             </p>
