5f31e4da135cd7a5d6d6201b962edb1ba8bfb10f
5hymon <245843@edu.p.lodz.pl>
2024-06-01 17:34:41 +0200

    Pierwsza wersja. Dodanie sekcji, opisu i prostego css

--- aplikacja_uzytkownika/src/components/BugReportSection.jsx
+++ aplikacja_uzytkownika/src/components/BugReportSection.jsx
@@ -1,9 +1,55 @@
-
+import { useState } from 'react';
+import '../styles/BugReportSection.css';
 
 function BugReportSection(){
-    return(
-        <div>
-            <h1 style={{color: "white"}}>Welcome in Bug Report Section!</h1>
+
+    const [section, setSection] = useState('todo');
+    const [description, setDescription] = useState('');
+    const [consent, setConsent] = useState(false);
+
+    const handleSubmit = () => {
+        if (description.trim() !== '') {
+            alert('Error reported successfully!');
+            setSection('todo');
+            setDescription('');
+            setConsent(false);
+        } else {
+            alert('Please enter a description of the error.');
+        }
+    };
+
+
+    return (
+        <div className="bug-report-section">
+            <label>
+                Sekcja:
+                <select value={section} onChange={(e) => setSection(e.target.value)}>
+                    <option value="log_in">Logowanie</option>
+                    <option value="to-do_list">To-Do List</option>
+                    <option value="events">Wydarzenia</option>
+                    <option value="menu_bar">Pasek Menu</option>
+                </select>
+            </label>
+            <br/>
+            <label>
+                Opis błędu:
+                <textarea
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                    placeholder="Wpisz opis napotkanego błędu"
+                />
+            </label>
+            <br/>
+            <label>
+                <input
+                    type="checkbox"
+                    checked={consent}
+                    onChange={(e) => setConsent(e.target.checked)}
+                />
+                Zgoda na wysyłanie danych o urządzeniu na którym wystąpił błąd
+            </label>
+            <br/>
+            <button onClick={handleSubmit}>Wyślij zgłoszenie</button>
         </div>
     );
 }
