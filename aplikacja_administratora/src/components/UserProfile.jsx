62cb435bd2d3586ec8932ca5be1045f07cfdbf09
dziwicz123 <michal.dziwisz@poczta.onet.pl>
2024-06-02 14:48:21 +0200

    Change UserProfile to interact with database

--- aplikacja_administratora/src/components/UserProfile.jsx
+++ aplikacja_administratora/src/components/UserProfile.jsx
@@ -1,9 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './UserProfile.css';
 import defaultPhoto from '../assets/default_user_picture.jpg';
 
 function UserProfile() {
+  const [user, setUser] = useState({
+    name: 'Loading...', // default name while loading
+    role: ''
+  });
   const [photo, setPhoto] = useState(defaultPhoto);
+
+  useEffect(() => {
+    fetchUserData();
+  }, []);
+
+  const fetchUserData = async () => {
+    try {
+      const response = await fetch('http://localhost:8081/api/user', {
+        method: 'GET',
+        credentials: 'include', // to ensure cookies are sent to handle session
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch user data');
+      }
+
+      const userData = await response.json();
+      setUser({
+        name: userData.fullName,
+        role: userData.type // assuming type is used for the role
+      });
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+      setUser({
+        name: 'Error fetching data',
+        role: ''
+      });
+    }
+  };
 
   const loadPhoto = (event) => {
     const reader = new FileReader();
@@ -19,20 +52,20 @@ function UserProfile() {
   };
 
   return (
-    <div>
-      <div className="photo_and_buttons">
-        <img src={photo} alt="admin photo" id="user_photo" />
-        <div className="user_profile_buttons">
-          <button id="user_profile_add_photo_btn" onClick={() => document.getElementById('fileInput').click()}>Dodaj zdjęcie</button>
-          <button id="user_profile_remove_photo_btn" onClick={removePhoto}>Usuń zdjęcie</button>
-          <input type="file" id="fileInput" style={{ display: 'none' }} accept="image/*" onChange={loadPhoto} />
+      <div>
+        <div className="photo_and_buttons">
+          <img src={photo} alt="user photo" id="user_photo" />
+          <div className="user_profile_buttons">
+            <button onClick={() => document.getElementById('fileInput').click()}>Add Photo</button>
+            <button onClick={removePhoto}>Remove Photo</button>
+            <input type="file" id="fileInput" style={{ display: 'none' }} accept="image/*" onChange={loadPhoto} />
+          </div>
+        </div>
+        <div className="user_info">
+          <p>{user.name}</p>
+          <p>{user.role}</p>
         </div>
       </div>
-      <div className="user_info">
-        <p>Krzysztof Kowalski</p>
-        <p>Administrator</p>
-      </div>
-    </div>
   );
 }
 
