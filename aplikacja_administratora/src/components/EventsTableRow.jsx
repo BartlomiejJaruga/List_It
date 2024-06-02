7b9a18fd2ba7dd54a6bf4e32dcad9d2cd52e5ed3
dziwicz123 <michal.dziwisz@poczta.onet.pl>
2024-06-02 14:48:48 +0200

    Change EventsPanel to interact with database

--- aplikacja_administratora/src/components/EventsTableRow.jsx
+++ aplikacja_administratora/src/components/EventsTableRow.jsx
@@ -1,36 +1,36 @@
+// EventsTableRow component update
 import React from 'react';
 import './EventsTableRow.css';
 
-function EventTableRow({ event, index, isSelected, onCheckboxChange, onStatusChange }) {
-  const handleStatusChange = (e) => {
-    const newStatus = e.target.value;
-    onStatusChange(event.id, newStatus);
-  };
+function EventsTableRow({ event, onCheckboxChange, onStatusChange }) {
+    const handleStatusChange = (e) => {
+        const newStatus = e.target.value;
+        onStatusChange(event.id, newStatus);
+    };
 
-  return (
-    <tr>
-      <td>{index}</td>
-      <td>{event.name}</td>
-      <td>{event.date}</td>
-      <td>{event.place}</td>
-      <td>{event.description}</td>
-      <td>{event.organizer}</td>
-      <td>
-        <select className='status_cell' value={event.status} onChange={handleStatusChange}>
-          <option value="Nowe">Nowe</option>
-          <option value="Zaakceptowano">Zaakceptowano</option>
-          <option value="Odrzucono">Odrzucono</option>
-        </select>
-      </td>
-      <td>
-        <input
-          type="checkbox"
-          checked={isSelected}
-          onChange={() => onCheckboxChange(event.id)}
-        />
-      </td>
-    </tr>
-  );
+    return (
+        <tr>
+            <td>{event.id}</td>
+            <td>{event.name}</td>
+            <td>{event.date}</td>
+            <td>{event.time}</td>
+            <td>{event.description}</td>
+            <td>
+                <select className='status_cell' value={event.status} onChange={handleStatusChange}>
+                    <option value="Nowe">Nowe</option>
+                    <option value="Zaakceptowano">Zaakceptowano</option>
+                    <option value="Odrzucono">Odrzucono</option>
+                </select>
+            </td>
+            <td>
+                <input
+                    type="checkbox"
+                    checked={event.isSelected}
+                    onChange={() => onCheckboxChange(event.id)}
+                />
+            </td>
+        </tr>
+    );
 }
 
-export default EventTableRow;
+export default EventsTableRow;
